df870d2f1910f832476b0209d7a4571b43be5061
dsh-ic <79240156+dsh-ic@users.noreply.github.com>
2021-02-18 16:01:41 +0800

    Update si7021.py

--- nxp1/si7021.py
+++ nxp1/si7021.py
@@ -6,7 +6,7 @@ bus = smbus2.SMBus(1)
 # SI7021 address, 0x40(64)
 
 si_addr=0x40
-define read_hu():
+define shidu():
   rh = bus.read_i2c_block_data(si_addr, 0xE5, 2) 
 # sends a 0xE5 command (measure RH, hold master mode) and read 2 bytes back
   time.sleep(0.1)
@@ -14,7 +14,7 @@ define read_hu():
   humidity = ((rh[0] * 256 + rh[1]) * 125 / 65536.0) - 6
   return humidty
 
-define read_temp():
+define temp():
   # Read data , 2 bytes, Temperature MSB first
   temp = bus.read_i2c_block_data(si_addr, 0xE3,2)
 #what really happens here is that master sends a 0xE3 command (measure temperature, hold master mode) and read 2 bytes back 
